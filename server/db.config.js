d1cd6a0b876bbb2c571cd886d5181e59775e2f44
liuzhe <liuzhe@qq.com>
2025-01-04 22:50:18 +0800

    Revert "feat"
    This reverts commit 298263cf6e30ef6d292d90b2b58a5d7f9aa5b24e.

--- server/db.config.js
+++ server/db.config.js
@@ -1,16 +1,16 @@
 function dbConfig() {
   return {
-    host: "192.168.0.104",
+    host: "192.168.0.105",
     port: 3306,
     user: "root",
     password: "123456",
     database: "chat-app",
-    connectionLimit: 1,
+    connectLimit: 1,
   };
 }
 
 // 用户表名
-const userTable = 'users'
+const userTable = 'user'
 
 module.exports = {
     dbConfig,
--- server/routes/login.js
+++ server/routes/login.js
@@ -2,23 +2,23 @@ const express = require("express");
 const mysql2 = require("mysql2");
 const router = express.Router();
 const {dbConfig, userTable} = require('../db.config')
-const {tokenManager} = require('../utils/utils')
 
 /* GET home page. */
 router.post("/login", async function (req, res) {
+  console.log(dbConfig)
+  console.log(req.body)
+  // a = "users";
   // // 创建连接池
-  const config = dbConfig();
-  const promisePool = mysql2.createPool(config).promise();
-  let users = await promisePool.query(
-    `SELECT * FROM ${userTable} WHERE account='${req.body.account}' AND password='${req.body.password}'`
-  ); //sql语句
-  if (users[0].length) {
-    res.send({ 
-      ok: 1,
-      token: tokenManager().encryptToken(users[0][0], '1h')
-     });
-  } else {
-    res.send({ ok: 0 });
-  }
+  // const config = handleConfig();
+  // const promisePool = mysql2.createPool(config).promise();
+  // let users = await promisePool.query(
+  //   `SELECT * FROM ${a} WHERE name='${req.body.account}' AND password='${req.body.password}'`
+  // ); //sql语句
+  // console.log(users[0]);
+  // if (users[0].length) {
+  //   res.send({ ok: 1 });
+  // } else {
+  //   res.send({ ok: 0 });
+  // }
 });
 module.exports = router;
