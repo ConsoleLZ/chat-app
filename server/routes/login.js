95c72f3678a6450bab2c536e9f0b41424ddda4f1
liuzhe <liuzhe@qq.com>
2025-01-04 23:10:56 +0800

    fix

--- server/routes/login.js
+++ server/routes/login.js
@@ -5,20 +5,19 @@ const {dbConfig, userTable} = require('../db.config')
 
 /* GET home page. */
 router.post("/login", async function (req, res) {
-  console.log(dbConfig)
-  console.log(req.body)
-  // a = "users";
   // // 创建连接池
-  // const config = handleConfig();
-  // const promisePool = mysql2.createPool(config).promise();
-  // let users = await promisePool.query(
-  //   `SELECT * FROM ${a} WHERE name='${req.body.account}' AND password='${req.body.password}'`
-  // ); //sql语句
-  // console.log(users[0]);
-  // if (users[0].length) {
-  //   res.send({ ok: 1 });
-  // } else {
-  //   res.send({ ok: 0 });
-  // }
+  const config = dbConfig();
+  const promisePool = mysql2.createPool(config).promise();
+  let users = await promisePool.query(
+    `SELECT * FROM ${userTable} WHERE account='${req.body.account}' AND password='${req.body.password}'`
+  ); //sql语句
+  if (users[0].length) {
+    res.send({ 
+      ok: 1,
+      token: tokenManager().encryptToken(users[0][0], '1h')
+     });
+  } else {
+    res.send({ ok: 0 });
+  }
 });
 module.exports = router;
