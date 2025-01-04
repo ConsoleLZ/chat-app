298263cf6e30ef6d292d90b2b58a5d7f9aa5b24e
liuzhe <liuzhe@qq.com>
2025-01-04 14:17:03 +0800

    feat

--- server/db.config.js
+++ server/db.config.js
@@ -1,16 +1,16 @@
 function dbConfig() {
   return {
-    host: "192.168.0.105",
+    host: "192.168.0.104",
     port: 3306,
     user: "root",
     password: "123456",
     database: "chat-app",
-    connectLimit: 1,
+    connectionLimit: 1,
   };
 }
 
 // 用户表名
-const userTable = 'user'
+const userTable = 'users'
 
 module.exports = {
     dbConfig,
