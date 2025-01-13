958b3a51f36c46c5bb60aaef06866d0d072dc419
liuzhe01 <liuzhe01@mucang.cn>
2025-01-13 16:29:13 +0800

    feat

--- pages/login/index.js
+++ pages/login/index.js
@@ -1,19 +1,38 @@
-import { defineComponent, reactive, toRefs } from 'vue';
+import { defineComponent, reactive, toRefs, ref } from 'vue';
 
 export default defineComponent({
 	setup() {
 		const state = reactive({
-			isPasswordType: true
+			isPasswordType: true,
+			account: '',
+			password: ''
 		});
 
+		const components = {
+			toastRef: ref(null)
+		};
+
 		const methods = {
-			onChangePasswordType(){
-				state.isPasswordType = !state.isPasswordType
+			onChangePasswordType() {
+				state.isPasswordType = !state.isPasswordType;
+			},
+			// 登录
+			onLogin() {
+				if(state.account === '' || state.password === ''){
+					components.toastRef.value.show({
+						type: 'warning',
+						title: '提示',
+						message: '账号或者密码不能为空'
+					});
+					return
+				}
+				console.log(state.account, state.password);
 			}
-		}
+		};
 
 		return {
 			...methods,
+			...components,
 			...toRefs(state)
 		};
 	}
--- pages/login/index.vue
+++ pages/login/index.vue
@@ -1,14 +1,17 @@
 <template>
 	<uv-image src="/static/logo.svg" mode="widthFix" class="logo"></uv-image>
 	<view class="main">
-		<uv-input class="input" clearable placeholder="请输入账号" prefixIcon="account-fill"></uv-input>
-		<uv-input class="input" style="margin-bottom: 100rpx;" :password="isPasswordType" clearable placeholder="请输入密码" prefixIcon="lock-fill">
+		<uv-input class="input" v-model="account" clearable placeholder="请输入账号" prefixIcon="account-fill"></uv-input>
+		<uv-input class="input" v-model="password" style="margin-bottom: 100rpx;" :password="isPasswordType" clearable placeholder="请输入密码" prefixIcon="lock-fill">
 			<template #suffix>
 				<uv-icon @click="onChangePasswordType" :name="isPasswordType ? 'eye-fill' : 'eye-off'"></uv-icon>
 			</template>
 		</uv-input>
-		<uv-button type="primary" text="确定"></uv-button>
+		<uv-button type="primary" @click="onLogin" text="登录"></uv-button>
+		<view class="or-text">——OR——</view>
+		<uv-button type="primary" :plain="true" text="注册"></uv-button>
 	</view>
+	<uv-toast ref="toastRef"></uv-toast>
 </template>
 
 <script src="./index.js"></script>
