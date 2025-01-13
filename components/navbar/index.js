792b80d5d6068b6bd8701dcb9151596c31f106b5
liuzhe01 <liuzhe01@mucang.cn>
2025-01-13 19:02:16 +0800

    fix

--- components/navbar/index.js
+++ components/navbar/index.js
@@ -1,4 +1,4 @@
-import { defineComponent } from 'vue';
+import { defineComponent, reactive, toRefs } from 'vue';
 
 export default defineComponent({
 	props: {
@@ -8,37 +8,17 @@ export default defineComponent({
 		}
 	},
 	setup() {
+		const state = reactive({
+			
+		});
+
 		const methods = {
-			// 显示弹框
-			async onShowDialog() {
-				// await uni.hideTabBar();
-				const dialogElement = uni.getElementById('dialog');
-				const dialogBoxElement = uni.getElementById('dialog-box');
-				dialogElement.style.setProperty('display', 'flex');
-				dialogBoxElement.style.setProperty('transform', 'scale(1)');
-			},
-			// 关闭弹框
-			async onCloseDialog() {
-				// await uni.showTabBar();
-				const dialogElement = uni.getElementById('dialog');
-				const dialogBoxElement = uni.getElementById('dialog-box');
-				dialogBoxElement.style.setProperty('transform', 'scale(0)');
-				setTimeout(() => {
-					dialogElement.style.setProperty('display', 'none');
-				}, 200);
-			},
-			// 添加好友
-			onAdd() {
-				setTimeout(() => {
-					uni.navigateTo({
-						url: '/pages-pack/add-friend/index'
-					});
-				}, 200);
-			}
+			
 		};
 
-        return {
-            ...methods
-        }
+		return {
+			...methods,
+			...toRefs(state)
+		};
 	}
 });
