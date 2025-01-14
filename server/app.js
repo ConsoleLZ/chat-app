01d3fb4a710d5bc40e6474298fafe8d81516772c
liuzhe01 <liuzhe01@mucang.cn>
2025-01-14 17:04:59 +0800

    feat:设置跨域

--- server/app.js
+++ server/app.js
@@ -10,6 +10,15 @@ var contactsRouter = require('./routes/contacts');
 
 var app = express();
 
+//设置允许跨域访问该服务.
+app.all('*', function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', '*');
+  res.header('Access-Control-Allow-Methods', '*');
+  res.header('Content-Type', 'application/json;charset=utf-8');
+  next();
+});
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
