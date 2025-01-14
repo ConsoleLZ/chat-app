2b13d8a8cddefadde18701a572259cce542bc642
liuzhe01 <liuzhe01@mucang.cn>
2025-01-14 11:23:13 +0800

    feat

---
+++ sub-pages/add-contact/index.vue
@@ -0,0 +1,13 @@
+<template>
+	<view class="navbar flex-center-row">
+		<uv-icon name="arrow-left"></uv-icon>
+		<view style="flex: 1;margin: 0 20rpx;">
+            <uv-input placeholder="搜索用户账号/名字/群号" prefixIcon="search"></uv-input>
+        </view>
+		<view>
+            <uv-text type="primary" text="清空"></uv-text>
+        </view>
+	</view>
+</template>
+
+<style lang="scss" src="./index.scss" scoped></style>
