ea5b40387fa63ba01ee364580e08af4cc6ec3a6b
liuzhe01 <liuzhe01@mucang.cn>
2025-01-16 19:02:47 +0800

    feat

--- pages/about/index.vue
+++ pages/about/index.vue
@@ -1,5 +1,26 @@
 <template>
 	<user-detail-comp />
+	<uv-list style="margin-top: 50rpx;">
+		<uv-list-item
+			title="收藏"
+			thumb="/static/about/star.png"
+			link
+			thumb-size="sm"
+		></uv-list-item>
+		<uv-list-item
+			title="设置"
+			thumb="/static/about/set.png"
+			link
+			thumb-size="sm"
+		></uv-list-item>
+		<uv-list-item
+			title="版本号: 1.0.0"
+			thumb="/static/about/version.png"
+			link
+			thumb-size="sm"
+		></uv-list-item>
+	</uv-list>
+	<uv-button style="width: 80%;margin: 100rpx auto;" type="error" text="退出登录"></uv-button>
 </template>
 
 <script src="./index.js"></script>
