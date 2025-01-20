76b1a9d9e5286fb596a203500ac2af48bb597bb5
liuzhe01 <liuzhe01@mucang.cn>
2025-01-20 10:59:25 +0800

    fix

--- App.vue
+++ App.vue
@@ -1,66 +1,60 @@
 <script>
-	import {
+import { postVerifiedStore } from '@/store/index.js';
+import { initSocket, listenPrivateMessage } from '@/utils/socketService.js';
+
+export default {
+	onLaunch: function () {
+		console.log('App Launch');
+	},
+	onShow: function () {
+		console.log('页面路由拦截');
+		// 页面路由拦截
+		uni.showLoading({
+			title: '加载中...',
+			mask: true
+		});
 		postVerifiedStore
-	} from '@/store/index.js';
-	import {
-		initSocket,
-		listenPrivateMessage
-	} from '@/utils/socketService.js';
-
-	export default {
-		onLaunch: function() {
-			console.log('App Launch');
-		},
-		onShow: function() {
-			console.log('页面路由拦截');
-			// 页面路由拦截
-			uni.showLoading({
-				title: '加载中...',
-				mask: true
-			});
-			postVerifiedStore
-				.post({
-					token: uni.getStorageSync('token')
-				})
-				.then(res => {
-					const ok = res.data?.ok;
-					if (!ok) {
-						uni.redirectTo({
-							url: '/pages/login/index'
-						});
-					} else {
-						// 初始化并连接到服务器
-						const socket = initSocket(uni.getStorageSync('userInfo'));
-
-						// 监听服务器消息
-						listenPrivateMessage(data => {
-							// 使用对象存储消息，以createTime作为key
-							const messages = uni.getStorageSync('messages') === '' ? {} : uni
-								.getStorageSync('messages');
-
-							// 检查消息是否已存在
-							if (!messages[data.createTime]) {
-								messages[data.createTime] = data;
-								uni.$emit('privateMessage', data);
-								uni.setStorageSync('messages', messages);
-							}
-						});
-					}
-				})
-				.catch(error => {
-					uni.showToast({
-						title: '服务器错误',
-						icon: 'error'
+			.post({
+				token: uni.getStorageSync('token')
+			})
+			.then(res => {
+				const ok = res.data?.ok;
+				if (!ok) {
+					uni.redirectTo({
+						url: '/pages/login/index'
 					});
-				})
-				.finally(() => {
-					uni.hideLoading();
+				} else {
+					// 初始化并连接到服务器
+					const socket = initSocket(uni.getStorageSync('userInfo'));
+
+					// 监听服务器消息
+					listenPrivateMessage(data => {
+						// 使用对象存储消息，以createTime作为key
+						const messages = uni.getStorageSync('messages') === '' ? {} : uni.getStorageSync('messages');
+
+						// 检查消息是否已存在
+						if (!messages[data.createTime]) {
+							messages[data.createTime] = data;
+							uni.$emit('privateMessage', data);
+							uni.setStorageSync('messages', messages);
+						}
+					});
+				}
+			})
+			.catch(error => {
+				uni.showToast({
+					title: '服务器错误',
+					icon: 'error'
 				});
-		},
-		onHide: function() {
-			console.log('App Hide');
-		}
-	};
+			})
+			.finally(() => {
+				uni.hideLoading();
+			});
+	},
+	onHide: function () {
+		console.log('App Hide');
+	}
+};
 </script>
 
 <style lang="scss" src="@/utils/utils.scss"></style>
--- server/db.config.js
+++ server/db.config.js
@@ -1,6 +1,6 @@
 function dbConfig() {
 	return {
-		host: '192.168.0.104',
+		host: '172.20.104.6',
 		port: 3306,
 		user: 'root',
 		password: '123456',
@@ -10,7 +10,7 @@ function dbConfig() {
 }
 
 const redisConfig = {
-	host: '192.168.0.104', // Redis服务器地址
+	host: '172.20.104.6', // Redis服务器地址
 	port: 6379, // Redis服务器端口
 	password: '123456' // Redis密码
 };
