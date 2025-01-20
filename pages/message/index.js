df2a26171204f9ce2458205fdfa68686b6fee58f
liuzhe01 <liuzhe01@mucang.cn>
2025-01-20 18:36:24 +0800

    fix

--- pages/message/index.js
+++ pages/message/index.js
@@ -12,21 +12,6 @@ export default defineComponent({
 			messageList: []
 		});
 
-		// 计算每个联系人的未读消息数量
-		const unreadCounts = computed(() => {
-			const counts = {};
-			const messages = uni.getStorageSync('messages') || {};
-			
-			Object.values(messages).forEach(message => {
-				if (!message.isView && message.receiverId === state.currentUserId) {
-					const contactId = message.senderId;
-					counts[contactId] = (counts[contactId] || 0) + 1;
-				}
-			});
-			
-			return counts;
-		});
-
 		const methods = {
 			// 跳转到聊天页
 			onJumpChat(item){
@@ -58,13 +43,23 @@ export default defineComponent({
 				const userInfo = uni.getStorageSync('userInfo');
 				state.currentUserId = userInfo.id;
 				
+				// 计算未读消息数量
+				const counts = {};
+				const messages = uni.getStorageSync('messages') || {};
+				
+				Object.values(messages).forEach(message => {
+					if (!message.isView && message.receiverId === state.currentUserId) {
+						const contactId = message.senderId;
+						counts[contactId] = (counts[contactId] || 0) + 1;
+					}
+				});
+
 				getContactsStore
 					.get({
 						userId: userInfo.id
 					})
 					.then(res => {
 						const contacts = res.data.contacts;
-						const messages = uni.getStorageSync('messages') || {};
 						const arr = [];
 						
 						Object.keys(messages).forEach(key => {
@@ -78,7 +73,7 @@ export default defineComponent({
 									state.messageList.push({
 										...contact,
 										content: messageItem.content,
-										unreadCount: unreadCounts.value[contact.contactUserId] || 0
+										unreadCount: counts[contact.contactUserId] || 0
 									});
 								}
 							});
@@ -97,15 +92,15 @@ export default defineComponent({
 			}
 		};
 
-		onShow(()=>{
-			state.messageList = []
+		onShow(() => {
+			// 清空并重新获取数据
+			state.messageList = [];
 			methods.getContactsData();
-		})
+		});
 
 		return {
 			...toRefs(state),
-			...methods,
-			unreadCounts
+			...methods
 		};
 	}
 });
