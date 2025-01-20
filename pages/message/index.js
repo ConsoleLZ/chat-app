fd3224eaa66fdacabf55f5af0859de777a704568
liuzhe01 <liuzhe01@mucang.cn>
2025-01-20 16:21:24 +0800

    feat

--- pages/message/index.js
+++ pages/message/index.js
@@ -1,8 +1,82 @@
-import { defineComponent } from 'vue';
+import { defineComponent, reactive, toRefs } from 'vue';
 import NavbarComp from '@/components/navbar/index.vue';
+import { getContactsStore } from '@/store/index.js';
 
 export default defineComponent({
 	components: {
 		NavbarComp
+	},
+	setup() {
+		const state = reactive({
+			messageList: []
+		});
+
+		const methods = {
+			formateMessages(arr, message) {
+				const index = arr.findIndex(item => {
+					if (item.senderId === message.senderId || item.senderId === message.receiverId) {
+						return true;
+					}
+				});
+				if (index !== -1) {
+					arr[index] = message;
+				} else {
+					arr.push(message);
+				}
+
+				return arr;
+			},
+			// 获取联系人数据
+			getContactsData() {
+				uni.showLoading({
+					title: '加载中'
+				});
+				const userInfo = uni.getStorageSync('userInfo');
+				getContactsStore
+					.get({
+						userId: userInfo.id
+					})
+					.then(res => {
+						const contacts = res.data.contacts
+						const messages = uni.getStorageSync('messages');
+						const arr = []
+						Object.keys(messages).forEach(key => {
+							methods.formateMessages(arr, messages[key]);
+						});
+
+						console.log(arr)
+						console.log(contacts)
+
+						arr.forEach(messageItem=>{
+							contacts.forEach(contact=>{
+								if(messageItem.receiverId === contact.contactUserId || messageItem.senderId === contact.contactUserId){
+									state.messageList.push({
+										...contact,
+										content: messageItem.content
+									})
+								}
+							})
+						})
+
+						console.log(state.messageList)
+					})
+					.catch(() => {
+						components.toastRef.value.show({
+							type: 'error',
+							title: '提示',
+							message: '服务器错误'
+						});
+					})
+					.finally(() => {
+						uni.hideLoading();
+					});
+			}
+		};
+
+		methods.getContactsData()
+
+		return {
+			...toRefs(state)
+		};
 	}
 });
--- pages/message/index.vue
+++ pages/message/index.vue
@@ -1,20 +1,20 @@
 <template>
 	<navbar-comp title="消息" />
 	<view class="list">
-		<view class="flex-center-row list-item">
+		<view class="flex-center-row list-item" v-for="(item, index) in messageList" :key="index">
 			<uv-avatar
 				size="90rpx"
 				style="background-color: #c1c1c1;margin-right: 12rpx;"
-				src="https://pic.imgdb.cn/item/665f06ac5e6d1bfa0573f8c5.jpg"
+				:src="item.avatar"
 			></uv-avatar>
 			<view style="flex: 1; position: relative; top: 5rpx">
 				<view class="flex-row">
-					<view class="title">123</view>
+					<view class="title">{{ item.name }}</view>
 					<text style="font-size: 20rpx; color: #c1c1c1">11:34</text>
 				</view>
 				<view class="flex-row">
 					<view class="message">
-						111
+						{{ item.content }}
 					</view>
 					<view class="badge flex-center-row">
 						<uv-badge
