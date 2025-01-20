60656d209871b899b17fe65ee432a335237496d4
liuzhe01 <liuzhe01@mucang.cn>
2025-01-20 14:36:20 +0800

    style

--- pages/message/index.vue
+++ pages/message/index.vue
@@ -2,15 +2,29 @@
 	<navbar-comp title="消息" />
 	<view class="list">
 		<view class="flex-center-row list-item">
-			<uv-avatar size="90rpx" style="margin-right: 15rpx" src="https://pic.imgdb.cn/item/665f06ac5e6d1bfa0573f8c5.jpg"></uv-avatar>
-			<view style="flex: 1">
-				<view class="title">123</view>
-				<text class="message">你好呀</text>
-			</view>
-			<view class="flex-center-column">
-				<text style="font-size: 20rpx;color: #c1c1c1;">11:34</text>
-				<view class="badge flex-center-row">
-					<uv-badge class="flex-center-row" numberType="overflow" type="error" max="99" :value="1"></uv-badge>
+			<uv-avatar
+				size="90rpx"
+				style="background-color: #c1c1c1;margin-right: 12rpx;"
+				src="https://pic.imgdb.cn/item/665f06ac5e6d1bfa0573f8c5.jpg"
+			></uv-avatar>
+			<view style="flex: 1; position: relative; top: 5rpx">
+				<view class="flex-row">
+					<view class="title">123</view>
+					<text style="font-size: 20rpx; color: #c1c1c1">11:34</text>
+				</view>
+				<view class="flex-row">
+					<view class="message">
+						111
+					</view>
+					<view class="badge flex-center-row">
+						<uv-badge
+							class="flex-center-row"
+							numberType="overflow"
+							type="error"
+							max="99"
+							:value="1"
+						></uv-badge>
+					</view>
 				</view>
 			</view>
 		</view>
