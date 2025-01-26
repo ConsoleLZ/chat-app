c2eac4303d4b141ecba2241ea8beb0d8919085e9
liuzhe <1804319025@qq.com>
2025-01-26 11:24:38 +0800

    feat

--- components/navbar/index.vue
+++ components/navbar/index.vue
@@ -26,12 +26,27 @@
 				hover-stay-time="150"
 			>
 				<uv-image
+					src="/static/add-groups.png"
+					width="36rpx"
+					mode="widthFix"
+					style="margin-right: 12rpx"
+				></uv-image>
+				<text style="width: 150rpx;">创建群聊</text>
+			</view>
+			<view
+				@click="onJumpSearchContact"
+				class="flex-center-row"
+				style="padding: 15rpx 0; width: 100%"
+				hover-class="hover-class-background"
+				hover-stay-time="150"
+			>
+				<uv-image
 					src="/static/add-friend.png"
 					width="36rpx"
 					mode="widthFix"
 					style="margin-right: 12rpx"
 				></uv-image>
-				<text>添加联系人</text>
+				<text style="width: 150rpx;">加好友/群</text>
 			</view>
 		</view>
 	</uv-overlay>
