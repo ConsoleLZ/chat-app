c3db1b3ef4d313918014040d2277818d6bb8e4ef
liuzhe <1804319025@qq.com>
2025-03-03 18:34:01 +0800

    fix

--- sub-pages/set-user-info/index.js
+++ sub-pages/set-user-info/index.js
@@ -1,27 +1,61 @@
-import {defineComponent, reactive, toRefs} from 'vue'
+import { defineComponent, reactive, toRefs } from 'vue';
+import { getUserInfoStore } from '@/store/index.js';
 
 export default defineComponent({
-    setup(){
-        const state = reactive({
-            formState: {
-                userInfo: {
-                    name: null,
-                    signature: null,
-                    tags: null,
-                }
-            },
-            rules: {
-                'userInfo.name': {
-                    type: 'string',
-                    required: true,
-                    message: '昵称不能为空',
-                    trigger: ['change']
-                }
-            },
-        })
+	setup() {
+		const state = reactive({
+            loading: false,
+			formState: {
+				userInfo: {
+                    avatar: null,
+					name: null,
+					signature: null,
+					tags: null
+				}
+			},
+			rules: {
+				'userInfo.name': {
+					type: 'string',
+					required: true,
+					message: '昵称不能为空',
+					trigger: ['change']
+				}
+			}
+		});
 
-        return {
-            ...toRefs(state)
-        }
-    }
-})
+		const methods = {
+			getData() {
+                state.loading = true
+				const userId = uni.getStorageSync('userInfo').id;
+
+				getUserInfoStore.get({ userId }).then(res => {
+					const userInfo = res.data.info[0];
+					
+                    const tags = userInfo.tags?.map(item=>{
+                        return {
+                            show: true,
+                            title: item
+                        }
+                    })
+                    state.formState.userInfo.name = userInfo.name
+                    state.formState.userInfo.avatar = userInfo.avatar
+                    state.formState.userInfo.signature = userInfo.signature
+                    state.formState.userInfo.tags = tags
+                    console.log(tags)
+				}).finally(()=>{
+                    state.loading = false
+                })
+			},
+            closeTag(index){
+                state.formState.userInfo.tags.splice(index, 1)
+            }
+		};
+
+		methods.getData();
+
+		return {
+			...toRefs(state),
+            ...methods
+		};
+	}
+});
--- sub-pages/set-user-info/index.vue
+++ sub-pages/set-user-info/index.vue
@@ -1,6 +1,26 @@
 <template>
 	<view style="padding: 0 20rpx">
-		<uv-form labelAlign="right" labelWidth="170rpx" labelPosition="left" :model="formState" :rules="rules" ref="form">
+		<uv-form
+			labelAlign="right"
+			labelWidth="170rpx"
+			labelPosition="left"
+			:model="formState"
+			:rules="rules"
+			ref="form"
+		>
+			<uv-form-item label="头像:" prop="userInfo.name" borderBottom>
+				<uv-avatar
+					v-if="formState.userInfo.avatar !== '' && formState.userInfo.avatar"
+					size="70rpx"
+					:src="formState.userInfo.avatar"
+				></uv-avatar>
+				<uv-avatar
+					v-else
+					:text="formState.userInfo.name?.slice(0, 1)"
+					fontSize="18"
+					size="70rpx"
+				></uv-avatar>
+			</uv-form-item>
 			<uv-form-item label="昵称:" prop="userInfo.name" borderBottom>
 				<uv-input v-model="formState.userInfo.name" border="none"></uv-input>
 			</uv-form-item>
@@ -8,10 +28,12 @@
 				<uv-input v-model="formState.userInfo.signature" border="none"></uv-input>
 			</uv-form-item>
 			<uv-form-item label="个性化标签:" prop="userInfo.signature" borderBottom>
-				<uv-tags text="标签" size="mini" closable :show="close1" @close="close1 = false"></uv-tags>
+				<uv-tags plain v-for="(item, index) in formState.userInfo.tags" :text="item.title" size="mini" closable :show="item.show" @close="closeTag(index)"></uv-tags>
 			</uv-form-item>
 		</uv-form>
 	</view>
+
+	<uv-loading-page :loading="loading" loading-text="加载中..." font-size="24rpx"></uv-loading-page>
 </template>
 
 <script src="./index.js"></script>
