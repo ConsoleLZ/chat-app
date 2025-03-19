f984a6945e69ca9260a7172513125e4e8a913d4c
liuzhe <1804319025@qq.com>
2025-03-19 10:03:51 +0800

    fix

--- sub-pages/user-detail/index.js
+++ sub-pages/user-detail/index.js
@@ -16,7 +16,7 @@ export default defineComponent({
 			// 跳转到聊天页
 			onJumpChat() {
 				uni.navigateTo({
-					url: `/sub-pages/chat-message/index?userId=${state.userInfo.id}`
+					url: `/sub-pages/chat-message/index?userId=${state.userId}`
 				});
 			}
 		};
