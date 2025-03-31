a5d1e73f85e0567cbcc4088b27e493e09c0e0ec6
liuzhe <1804319025@qq.com>
2025-04-01 02:18:05 +0800

    feat

--- pages/moments/index.vue
+++ pages/moments/index.vue
@@ -1,3 +1,21 @@
 <template>
-	<div></div>
+	<view class="page">
+		<view>
+			<view class="flex-row margin-bottom-20" style="gap: 20rpx;">
+				<uv-avatar src="https://via.placeholder.com/200x200.png/2878ff" shape="square"></uv-avatar>
+				<view>
+					<view>用户名字</view>
+					<uv-text type="info" text="发布时间：2021-01-01 12:00"></uv-text>
+				</view>
+			</view>
+			<view class="margin-bottom-20">用户内容用户内容用户内容用户内容用户内容用户内容用户内容用户内容用户内容用户内容用户内容</view>
+			<view class="icon-box">
+				<uv-icon size="20" name="/static/moments/1.png"></uv-icon>
+				<uv-icon size="20" name="/static/moments/3.png"></uv-icon>
+			</view>
+			<uv-text type="info" text="小哲赞了、阿松地哦赞了"></uv-text>
+		</view>
+	</view>
 </template>
+
+<style lang="scss" src="./index.scss" scoped></style>
