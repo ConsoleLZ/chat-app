01dd157e044fbaa69f6d5dfa3042157454562359
liuzhe <1804319025@qq.com>
2025-04-01 03:07:36 +0800

    fix

--- sub-pages/publish-moments/index.js
+++ sub-pages/publish-moments/index.js
@@ -3,11 +3,28 @@ import { defineComponent, reactive, toRefs } from 'vue';
 export default defineComponent({
 	setup() {
 		const state = reactive({
-			content: ''
+			content: '',
+			fileList: []
 		});
 
+		const methods = {
+			afterRead(e) {
+				const fileList = e.file.map(item => {
+					return {
+						...item,
+						status: 'uploading',
+						message: '上传中'
+					};
+				});
+                fileList.forEach(item => {
+                  state.fileList.push(item)  
+                })
+			}
+		};
+
 		return {
-			...toRefs(state)
+			...toRefs(state),
+			...methods
 		};
 	}
 });
--- sub-pages/publish-moments/index.vue
+++ sub-pages/publish-moments/index.vue
@@ -1,12 +1,20 @@
 <template>
-    <view class="page">
-        <uv-navbar title="发布动态">
-            <template #right>
-                <uv-button type="primary" customStyle="height: 55rpx;" text="发布"></uv-button>
-            </template>
-        </uv-navbar>
-        <uv-textarea v-model="content" :height="180" :maxlength="200" count placeholder="分享新鲜事..."></uv-textarea>
-    </view>
+	<view class="page">
+		<uv-navbar title="发布动态">
+			<template #right>
+				<uv-button type="primary" customStyle="height: 55rpx;" text="发布"></uv-button>
+			</template>
+		</uv-navbar>
+		<uv-textarea
+			v-model="content"
+			:height="180"
+			:maxlength="200"
+			count
+			placeholder="分享新鲜事..."
+			class="margin-bottom-20"
+		></uv-textarea>
+		<uv-upload :fileList="fileList" multiple :maxCount="10" @afterRead="afterRead"></uv-upload>
+	</view>
 </template>
 
 <script src="./index.js"></script>
