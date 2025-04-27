18c691d8162915087ae491305f35f50da52fcfd9
liuzhe <1804319025@qq.com>
2025-04-27 22:21:43 +0800

    feat

--- sub-pages/my-expression/index.vue
+++ sub-pages/my-expression/index.vue
@@ -5,6 +5,11 @@
 			:maxCount="10"
 			@afterRead="afterRead"
 		></uv-upload>
+        <view class="expression-list">
+            <uv-image src="https://cdn.uviewui.com/uview/album/1.jpg" width="220rpx" height="220rpx"></uv-image>
+            <uv-image src="https://cdn.uviewui.com/uview/album/1.jpg" width="220rpx" height="220rpx"></uv-image>
+            <uv-image src="https://cdn.uviewui.com/uview/album/1.jpg" width="220rpx" height="220rpx"></uv-image>
+        </view>
 	</view>
 </template>
 
