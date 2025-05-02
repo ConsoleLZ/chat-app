25842799bbb982c1fe628d9802cc21f20084d83d
liuzhe <1804319025@qq.com>
2025-05-02 22:42:37 +0800

    fix

--- sub-pages/search-contact/index.vue
+++ sub-pages/search-contact/index.vue
@@ -17,67 +17,68 @@
 				<uv-text @click="onClear" type="primary" text="清空"></uv-text>
 			</view>
 		</view>
-		<view class="search-list" v-if="users">
-			<uv-text style="margin-bottom: 24rpx" size="24rpx" type="info" text="用户"></uv-text>
-			<view class="flex-center-row search-list-item" v-for="item in users" :key="item.id">
-				<uv-avatar
-					v-if="item.avatar !== '' && item.avatar"
-					size="90rpx"
-					style="margin-right: 15rpx"
-					:src="item.avatar"
-				></uv-avatar>
-				<uv-avatar
-					v-else
-					:text="item.name?.slice(0, 1)"
-					fontSize="18"
-					size="90rpx"
-					randomBgColor
-					style="margin-right: 15rpx"
-				></uv-avatar>
-				<view style="flex: 1">
-					<view class="title">{{ item.name }}</view>
-					<view class="flex-row">
-						<view class="tag">未知</view>
+		<view class="search-list">
+			<!-- 用户 -->
+			<view v-if="users">
+				<uv-text style="margin-bottom: 24rpx" size="24rpx" type="info" text="用户"></uv-text>
+				<view class="flex-center-row search-list-item" v-for="item in users" :key="item.id">
+					<uv-avatar
+						v-if="item.avatar !== '' && item.avatar"
+						size="90rpx"
+						style="margin-right: 15rpx"
+						:src="item.avatar"
+					></uv-avatar>
+					<uv-avatar
+						v-else
+						:text="item.name?.slice(0, 1)"
+						fontSize="18"
+						size="90rpx"
+						randomBgColor
+						style="margin-right: 15rpx"
+					></uv-avatar>
+					<view style="flex: 1">
+						<view class="title">{{ item.name }}</view>
+						<view class="flex-row">
+							<view class="tag">未知</view>
+						</view>
 					</view>
+					<uv-button
+						@click="onApplicationAdd(item.id)"
+						v-if="!contactUserIdList.includes(item.id) && userId !== item.id"
+						type="primary"
+						text="添加"
+					></uv-button>
+					<uv-button v-else type="primary" disabled text="已添加"></uv-button>
 				</view>
-				<uv-button
-					@click="onApplicationAdd(item.id)"
-					v-if="!contactUserIdList.includes(item.id) && userId !== item.id"
-					type="primary"
-					text="添加"
-				></uv-button>
-				<uv-button v-else type="primary" disabled text="已添加"></uv-button>
 			</view>
-		</view>
-		<view class="search-list" v-if="groups">
-			<uv-text style="margin-bottom: 24rpx" size="24rpx" type="info" text="群聊"></uv-text>
-			<view class="flex-center-row search-list-item" v-for="item in groups" :key="item.id">
-				<uv-avatar
-					v-if="item.avatar !== '' && item.avatar"
-					size="90rpx"
-					style="margin-right: 15rpx"
-					:src="item.avatar"
-				></uv-avatar>
-				<uv-avatar
-					v-else
-					:text="item.name?.slice(0, 1)"
-					fontSize="18"
-					size="90rpx"
-					randomBgColor
-					style="margin-right: 15rpx"
-				></uv-avatar>
-				<view style="flex: 1">
-					<view class="title">{{ item.name }}</view>
+			<!-- 群聊 -->
+			<view v-if="groups">
+				<uv-text style="margin-bottom: 24rpx" size="24rpx" type="info" text="群聊"></uv-text>
+				<view class="flex-center-row search-list-item" v-for="item in groups" :key="item.id">
+					<uv-avatar
+						v-if="item.avatar !== '' && item.avatar"
+						size="90rpx"
+						style="margin-right: 15rpx"
+						:src="item.avatar"
+					></uv-avatar>
+					<uv-avatar
+						v-else
+						:text="item.name?.slice(0, 1)"
+						fontSize="18"
+						size="90rpx"
+						randomBgColor
+						style="margin-right: 15rpx"
+					></uv-avatar>
+					<view style="flex: 1">
+						<view class="title">{{ item.name }}</view>
+					</view>
+					<uv-button type="primary" text="申请"></uv-button>
+					<!-- <uv-button v-else type="primary" disabled text="已添加"></uv-button> -->
 				</view>
-				<uv-button
-					type="primary"
-					text="申请"
-				></uv-button>
-				<!-- <uv-button v-else type="primary" disabled text="已添加"></uv-button> -->
 			</view>
 		</view>
 	</view>
-	
+
 	<toast-comp ref="toastRef"></toast-comp>
 </template>
 
